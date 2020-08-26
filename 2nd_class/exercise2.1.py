6a2b914aafd0954b2e82745d98e4e4fc2ef77b5e
Marcio Souza Filho <marciorasf@gmail.com>
2020-08-25 21:17:40 -0300

    fix exercise 2.1 and purge

--- 2nd_class/exercise2.1.py
+++ 2nd_class/exercise2.1.py
@@ -12,29 +12,27 @@ import utils
 sys.path.insert(1, "../utils")
 from separateIndexesByRatio import separateIndexesByRatio
 
-
 # %% Initialize parameters
 adalineDimension = 3
-tol = 1e-5
+tol = 1e-15
 maxIterations = 100
 trainSamplesRatio = 0.9
 nSamplesPerGroup = 200
 
 # %% Initialize data
-firstGroupSigma = 0
-firstGroupMean = [2, 2]
-firstGroupInput = np.random.multivariate_normal(
-    firstGroupMean, [[firstGroupSigma, 0], [0, firstGroupSigma]], nSamplesPerGroup
-)
+firstGroupSigma = 0.4
+firstGroupMean = 2
+
+firstGroupInput = np.random.normal(firstGroupMean, firstGroupSigma, (nSamplesPerGroup,2))
 firstGroupInput = utils.addConstantTerm(firstGroupInput)
+
 firstGroupOutput = [1] * nSamplesPerGroup
 
 secondGroupSigma = 0.4
-secondGroupMean = [4, 4]
-secondGroupInput = np.random.multivariate_normal(
-    secondGroupMean, [[secondGroupSigma, 0], [0, secondGroupSigma]], nSamplesPerGroup
-)
+secondGroupMean = 4
+secondGroupInput = np.random.normal(secondGroupMean, secondGroupSigma, (nSamplesPerGroup,2))
 secondGroupInput = utils.addConstantTerm(secondGroupInput)
+
 secondGroupOutput = [-1] * nSamplesPerGroup
 
 inputData = np.concatenate((firstGroupInput, secondGroupInput))
@@ -46,7 +44,6 @@ trainIndexes, testIndexes = separateIndexesByRatio(
 random.shuffle(trainIndexes)
 
 # %% Initialize and Train Adaline
-
 
 adaline = Adaline([0] * (adalineDimension), 0.1, lambda x: 1 if x >= 0 else -1)
 
@@ -65,43 +62,32 @@ print(f"Mean Squared Error: {testResult}")
 # %% Plot
 adalineApproxYArr = adaline.evaluate(inputData)
 
-# fig1 = px.scatter(x=inputData[:, 0], y=inputData[:, 1], color=adalineApproxYArr)
-
-# fig1.show()
-
-# fig2 = px.scatter(x=inputData[:, 0], y=inputData[:, 1], color=outputData)
-
-# fig2.show()
-
-# fig3 = px.line(x=xPlan, y=yPlan)
-
-# fig3.show()
-
 weights = adaline.getWeights()
-
 
 def hyperPlan(x):
     return -(weights[0] * x + weights[2]) / weights[1]
 
-
-xPlan = np.linspace(0, 6, 100)
+xPlan = np.linspace(0.5, 5.5, 100)
 yPlan = np.vectorize(hyperPlan)(xPlan)
 
-fig = go.Figure()
+fig = make_subplots(x_title="x", y_title="y")
 
 # Add traces
 fig.add_trace(go.Scatter(
-    x=inputData[:, 0], 
-    y=inputData[:, 1], 
+    x=firstGroupInput[:, 0], 
+    y=firstGroupInput[:, 1], 
     mode="markers", 
-    marker=dict(color=adalineApproxYArr, colorscale='Viridis')
+    name="Grupo 1"
     ))
-fig.add_trace(go.Scatter(x=xPlan, y=yPlan, mode="lines",))
+fig.add_trace(go.Scatter(
+    x=secondGroupInput[:, 0], 
+    y=secondGroupInput[:, 1], 
+    mode="markers", 
+    name="Grupo 2"
+    ))
+fig.add_trace(go.Scatter(x=xPlan, y=yPlan, mode="lines",name="Hiperplano separador"))
 
 fig.show()
 
-
 # %%
 adaline.printDetails()
-
-# %%
