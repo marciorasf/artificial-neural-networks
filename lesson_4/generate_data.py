6a0e2446b96a078b398566389ae5f19d1fcf28b0
Marcio Souza Filho <marciorasf@gmail.com>
2020-09-04 21:38:15 -0300

    elm working

---
+++ lesson_4/generate_data.py
@@ -0,0 +1,66 @@
+# %%
+import numpy as np
+import pandas as pd
+
+
+def generateData(mean, sigma, size, transformFunc):
+    xData = np.random.normal(mean, sigma, (size, 2))
+    xDim = xData.shape[1]
+    xColumns = [f"x{i}" for i in range(xDim)]
+
+    yData = np.atleast_2d(np.array(list(map(transformFunc, xData))))
+    if yData.shape[0] == 1:
+        yData = yData.T
+
+    yDim = yData.shape[1]
+    yColumns = [f"y{i}" for i in range(yDim)]
+
+    data = np.c_[xData, yData]
+    columns = xColumns + yColumns
+
+    dfData = pd.DataFrame(data, columns=columns)
+    return dfData
+
+
+def writeDataToCSV(df, filename):
+    df.to_csv(filename)
+
+
+def readDataFromCSV(filename):
+    data = pd.read_csv(filename, index_col=0)
+    return data
+
+
+def generateExercise1Data(pointsPerGroup):
+    firstGroupMean = [2, 2]
+    firstGroupSigma = 0.4
+
+    secondGroupMean = [4, 4]
+    secondGroupSigma = 0.4
+
+    df1 = generateData(firstGroupMean, firstGroupSigma, pointsPerGroup, lambda x: 1)
+    df2 = generateData(secondGroupMean, secondGroupSigma, pointsPerGroup, lambda x: -1)
+
+    df1["group"] = 1
+    df2["group"] = 2
+
+    df = pd.concat([df1, df2])
+
+    return df
+
+def generateExercise2Data(pointsPerGroup):
+    firstGroupMean = [2, 2]
+    firstGroupSigma = 0.4
+
+    secondGroupMean = [4, 4]
+    secondGroupSigma = 0.4
+
+    df1 = generateData(firstGroupMean, firstGroupSigma, pointsPerGroup, lambda x: 1)
+    df2 = generateData(secondGroupMean, secondGroupSigma, pointsPerGroup, lambda x: -1)
+
+    df1["group"] = 1
+    df2["group"] = 2
+
+    df = pd.concat([df1, df2])
+
+    return df
